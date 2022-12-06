70b2a9c2674ff9eee3c3b25f0a17a5eed775c8b4
nikki2107 <71547789+nikki2107@users.noreply.github.com>
2022-12-06 19:40:13 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ star_table = soup.find_all('table')
 print(len(star_table))
 
 temp_list=[]
-table_rows=star_table[4].find_all('tr')
+table_rows=star_table[7].find_all('tr')
 for tr in table_rows:
     td=tr.find_all('td')
     row=[i.text.rstrip() for i in td]
